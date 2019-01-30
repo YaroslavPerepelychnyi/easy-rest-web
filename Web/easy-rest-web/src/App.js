eae784c593d15709b56854efe3b35da615aff814
Yaroslav Perepelychnyi <Yaroslav_Perepelychnyi@epam.com>
2019-01-30 16:22:20 +0200

    initial dev commit

--- Web/easy-rest-web/src/App.js
+++ Web/easy-rest-web/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Easy rest by Yaroslav
+            Easy rest by Yaroslav Perepelychnyi
           </p>
         </header>
       </div>
